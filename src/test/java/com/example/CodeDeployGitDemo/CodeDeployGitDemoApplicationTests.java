0befbcf0449c43c9e198cb3271564c311e857cf6
Elhanes <37682794+Elhanes@users.noreply.github.com>
2021-05-13 17:55:36 +0900

    demo

---
+++ src/test/java/com/example/CodeDeployGitDemo/CodeDeployGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeDeployGitDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDeployGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
